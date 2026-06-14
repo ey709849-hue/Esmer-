c627e057f43442b210a19052f31dd9c49f6048fc
ey709849-hue <ey709849@gmail.com>
2026-06-14 03:03:10 +0000

    Update reklam_bot.py

--- reklam_bot.py
+++ reklam_bot.py
@@ -5,18 +5,27 @@ import requests
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_IDS = os.environ["CHAT_IDS"].split(",")
 
-# Buraları kendin doldur:
-IMAGE_URL = "BURAYA_RESIM_LINKI"
-CAPTION = "BURAYA_ACIKLAMA_VE_WHATSAPP_LINKI"
+IMAGE_FILE_ID = "AgACAgQAAxkBAAMEai4T-a20_uR-6NDKnxmjAitfAY0AAqsMaxsnnHFRhQsByueTE0QBAAMCAAN5AAM8BA"
+CAPTION = (
+    "🔥 ÖDEME ELDEN NAKİT\n"
+    "İsmim esmer\n\n"
+    "Yaşım 25\n\n\n\"
+    " Boyum  160\n\n\n\n\"
+    "Kilom  50\n\n\n\n\n\"
+    "📲 WhatsApp: https://wa.me/90506069369"
+)
 
 INTERVAL_SECONDS = 300  # 5 dakika
 
 def send_ad():
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
     for chat_id in CHAT_IDS:
-        data = {"chat_id": chat_id.strip(), "caption": CAPTION}
-        params = {"photo": IMAGE_URL}
-        resp = requests.post(url, data=data, params=params)
+        data = {
+            "chat_id": chat_id.strip(),
+            "photo": IMAGE_FILE_ID,
+            "caption": CAPTION,
+        }
+        resp = requests.post(url, data=data)
         print(chat_id, resp.status_code, resp.text)
         time.sleep(2)
 
