a3088b99e46f9ece4e6a31acf088fd8fed0a2997
ey709849-hue <ey709849@gmail.com>
2026-06-14 03:41:34 +0300

    Create reklam_bot.py

---
+++ reklam_bot.py
@@ -0,0 +1,25 @@
+import os
+import time
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_IDS = os.environ["CHAT_IDS"].split(",")
+
+IMAGE_URL = "BURAYA_RESIM_LINKI"
+CAPTION = "BURAYA_ACIKLAMA_VE_WHATSAPP_LINKI"
+
+INTERVAL_SECONDS = 300
+
+def send_ad():
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
+    for chat_id in CHAT_IDS:
+        data = {"chat_id": chat_id.strip(), "caption": CAPTION}
+        params = {"photo": IMAGE_URL}
+        resp = requests.post(url, data=data, params=params)
+        print(chat_id, resp.status_code, resp.text)
+        time.sleep(2)
+
+if __name__ == "__main__":
+    while True:
+        send_ad()
+        time.sleep(INTERVAL_SECONDS)
